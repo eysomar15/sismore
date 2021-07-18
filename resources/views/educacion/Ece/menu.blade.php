327feb7ace0c9a8a3a4de7936a253c50cc37bd31
rRoblesr <raroblesr@gmail.com>
2021-07-18 10:21:47 -0500

    update18071021

--- resources/views/educacion/Ece/menu.blade.php
+++ resources/views/educacion/Ece/menu.blade.php
@@ -12,87 +12,44 @@
                 </div>
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-12">
-                            <div class="table-responsive">
-                                <table class="table table-bordered mb-0">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>CATEGORIA</th>
-                                            <th>INDICADORES</th>
-                                            <th>FUENTE</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td rowspan="3">CULMINACIÓN DE LA EDUCACIÓN BÁSICA Y SUPERIOR</td>
-                                            <td><a href="#">Tasa de conclusión, primaria, grupo de edades 12-13 años (% del total)</a></td>
-                                            <td rowspan="3">Bases de datos de la Encuesta Nacional de Hogares del Instituto Nacional 
-                                                de Estadística e Informática - INEI</td>
-                                        </tr>
-                                        <tr>
-                                            <td>2</td>
-                                            <td><a href="#">Tasa de conclusión, secundaria, grupo de edades 17-18 años (% del total)</a></td>
-                                        </tr>
-                                        <tr>
-                                            <td>3</td>
-                                            <td><a href="#">Tasa de conclusión, educación superior, grupo de edades 22-24 años (% del total)</a></td>
-                                        </tr>
-                                        <tr>
-                                            <td>4</td>
-                                            <td rowspan="4">LOGROS DE APRENDIZAJE</td>
-                                            <td><a href="{{route('ece.indicador.4')}}">Alumnos que logran los aprendizajes del grado (% de alumnos de 2° grado de primaria participantes en evaluaciones censal)</a></td>
-                                            <td rowspan="4">Base de datos de la Evaluación Censal De Estudiantes (ECE) del Ministerio 
-                                                de Educación-Oficina de Medición de Calidad de los Aprendizajes</td>
-                                        </tr>
-                                        <tr>
-                                            <td>5</td>
-                                            <td><a href="{{route('ece.indicador.5')}}">Alumnos que logran los aprendizajes del grado (% de alumnos de 2° grado de secundaria participantes en evaluación censal)</a></td>
-                                        </tr>
-                                        <tr>
-                                            <td>6</td>
-                                            <td><a href="{{route('ece.indicador.6')}}">Alumnos que logran los aprendizajes del grado (% de alumnos de 4° grado de primaria participantes en evaluación censal)</a></td>
-                                        </tr>
-                                        <tr>
-                                            <td>7</td>
-                                            <td><a href="{{route('ece.indicador.7')}}">Alumnos de EIB que logran los aprendizajes del 4° grado en lengua materna y en castellano como segunda lengua.</a></td>
-                                        </tr>
-                                        <tr>
-                                            <td>8</td>
-                                            <td rowspan="3">ACCESO A LA EDUCACIÓN</td>
-                                            <td><a href="#">Tasa neta de matrícula, educación inicial (% de población con edades de 3-5 años)</a></td>
-                                            <td rowspan="3">Base de datos del Sistema de Información de Apoyo a la Gestión de la 
-                                                Institución Educativa (SIAGIE).</td>
-                                        </tr>
-                                        <tr>
-                                            <td>9</td>
-                                            <td><a href="#">Tasa neta de matrícula, educación primaria (% de población con edades de 6-11 años)</a></td>
-                                        </tr>
-                                        <tr>
-                                            <td>10</td>
-                                            <td><a href="#">Tasa neta de matrícula, educación secundaria (% de población con edades de 12-16 años)</a></td>
-                                        </tr>
-                                        <tr>
-                                            <td>11</td>
-                                            <td rowspan="3">PROFESORES Y PROMOTORAS EDUCATIVAS</td>
-                                            <td><a href="#">Profesores titulados, inicial (% del total)</a></td>
-                                            <td rowspan="3">Nexus</td>
-                                        </tr>
-                                        <tr>
-                                            <td>12</td>
-                                            <td><a href="#">Profesores titulados, primaria (% del total)</a></td>
-                                        </tr>
-                                        <tr>
-                                            <td>13</td>
-                                            <td><a href="#">Profesores titulados, secundaria (% del total)</a></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-
+                        <div class="col-lg-12">
+                            <ul class="mb-0">
+                                <li>CULMINACIÓN DE LA EDUCACIÓN BÁSICA Y SUPERIOR</li>
+                                <ul>
+                                    <li>1.-<a href="#">Tasa de conclusión, primaria, grupo de edades 12-13 años (% del total)</a></li>
+                                    <li>2.-<a href="#">Tasa de conclusión, secundaria, grupo de edades 17-18 años (% del total)</a></li>
+                                    <li>3.-<a href="#">Tasa de conclusión, educación superior, grupo de edades 22-24 años (% del total)</a></li>
+                                    <li>FUENTE: Bases de datos de la Encuesta Nacional de Hogares del Instituto Nacional 
+                                        de Estadística e Informática - INEI</li>
+                                </ul>
+                                <li>LOGROS DE APRENDIZAJE</li>
+                                <ul>
+                                    <li>4.-<a href="{{route('ece.indicador.4')}}">Alumnos que logran los aprendizajes del grado (% de alumnos de 2° grado de primaria participantes en evaluaciones censal)</a></li>
+                                    <li>5.-<a href="{{route('ece.indicador.5')}}">Alumnos que logran los aprendizajes del grado (% de alumnos de 2° grado de secundaria participantes en evaluación censal)</a></li>
+                                    <li>6.-<a href="{{route('ece.indicador.6')}}">Alumnos que logran los aprendizajes del grado (% de alumnos de 4° grado de primaria participantes en evaluación censal)</a></li>
+                                    <li>7.-<a href="{{route('ece.indicador.7')}}">Alumnos de EIB que logran los aprendizajes del 4° grado en lengua materna y en castellano como segunda lengua.</a></li>
+                                    <li>FUENTE: Base de datos de la Evaluación Censal De Estudiantes (ECE) del Ministerio 
+                                        de Educación-Oficina de Medición de Calidad de los Aprendizajes</li>
+                                </ul>
+                                <li>ACCESO A LA EDUCACIÓN</li>
+                                <ul>
+                                    <li>8.-<a href="#">Tasa neta de matrícula, educación inicial (% de población con edades de 3-5 años)</a></li>
+                                    <li>9.-<a href="#">Tasa neta de matrícula, educación primaria (% de población con edades de 6-11 años)</a></li>
+                                    <li>10.-<a href="#">Tasa neta de matrícula, educación secundaria (% de población con edades de 12-16 años)</a></li>
+                                    <li>FUENTE: Base de datos del Sistema de Información de Apoyo a la Gestión de la 
+                                        Institución Educativa (SIAGIE).</li>
+                                </ul>
+                                <li>PROFESORES Y PROMOTORAS EDUCATIVAS</li>
+                                <ul>
+                                    <li>11.-<a href="#">Profesores titulados, inicial (% del total)</a></li>
+                                    <li>12.-<a href="#">Profesores titulados, primaria (% del total)</a></li>
+                                    <li>13.-<a href="#">Profesores titulados, secundaria (% del total)</a></li>
+                                    <li>FUENTE: Nexus</li>
+                                </ul>
+                            </ul>
                         </div>
                     </div>
+                    
                 </div>
             </div>
         </div>
