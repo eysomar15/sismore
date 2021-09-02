359490bb5d9b3c230681c78cf0636a1d427033c5
rRoblesr <raroblesr@gmail.com>
2021-09-02 18:42:33 -0500

    ddddd

--- app/Http/Controllers/Educacion/IndicadorController.php
+++ app/Http/Controllers/Educacion/IndicadorController.php
@@ -293,10 +293,7 @@ class IndicadorController extends Controller
             case 24: //PROGRAMA NACIONAL DE SANEAMIENTO RURAL
                 $indicador = Indicador::find($indicador_id);
                 $title = $indicador->nombre;
-
-                //$cp=IndicadorRepositorio::cabecera1();
-                //$cp = DB::table('viv_datass as v1')->groupBy('servicio_agua_continuo')->get([DB::raw('count(servicio_agua_continuo)')]);                
-                //$cp = DB::table('viv_datass as v1')->groupBy('realiza_cloracion_agua')->get([DB::raw('count(realiza_cloracion_agua)')]);
+                
                 $provincias = Ubigeo::whereRaw('LENGTH(codigo)=4')->get();
                 $breadcrumb = [['titulo' => 'Relacion de indicadores', 'url' => route('Clasificador.menu', '02')], ['titulo' => 'Indicadores', 'url' => '']];
                 return view('parametro.indicador.vivcat1', compact('title', 'breadcrumb','provincias','indicador_id'));
