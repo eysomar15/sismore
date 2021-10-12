99e6a6398b24697aceced89fc3679fa03c2f324f
eysomar15 <eysomar_gambini@hotmail.com>
2021-10-12 09:50:45 -0500

    1210

---
+++ resources/views/educacion/Matricula/Inicio.blade.php
@@ -0,0 +1,1 @@
+<h1>resumen gamb</h1>
---
+++ resources/views/educacion/Matricula/grafico.blade.php
@@ -0,0 +1,13 @@
+<div class="content">
+   <div class="card">
+       <div class="card-body">
+           <div class="row">
+               <div class="col-md-12">      
+                   <div id="barra1">       
+                       {{-- se carga con el scrip lineas abajo --}}
+                   </div> 
+               </div> 
+           </div> 
+       </div> 
+   </div> 
+</div> 
