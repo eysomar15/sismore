2a4dfe578cf1777b596dfeb315682efc61bd2b09
rRoblesr <raroblesr@gmail.com>
2021-10-26 12:34:00 -0500

    AFASDASDSA

--- app/Repositories/Vivienda/EmapacopsaRepositorio.php
+++ app/Repositories/Vivienda/EmapacopsaRepositorio.php
@@ -46,6 +46,27 @@ class EmapacopsaRepositorio
                     ->select(DB::raw('xx.servicio as name'), DB::raw('cast(SUM(xx.conteo) as SIGNED) as y'))
                     ->groupBy('xx.servicio')
                     ->get();
+                $query['categoriaconagua'] = DB::table(DB::raw('(select v7.nombre as categoria, count(v1.id) as "con_agua",cast(SUM(v1.unid_uso) as SIGNED) as "unid_uso"
+                from `viv_emapacopsa` as `v1` 
+                inner join `viv_tipo_servicio` as `v2` on `v2`.`id` = `v1`.`tipo_servicio_id`         
+                inner join `viv_estado_conexion` as `v3` on `v3`.`id` = `v1`.`estado_conexion_id`   
+                inner join `viv_manzana` as `v4` on `v4`.`id` = `v1`.`manzana_id`  
+                inner join `viv_sector` as `v5` on `v5`.`id` = `v4`.`sector_id` 
+                inner join `par_ubigeo` as `v6` on `v6`.`id` = `v5`.`ubigeo_id` 
+                inner join `viv_sub_categoria` as `v7` on `v7`.`id` = `v1`.`sub_categoria_id`    
+                WHERE `v1`.`importacion_id`=' . $importacion_id . ' and `v1`.`tipo_servicio_id` in(1,2) 
+                group by `v7`.`nombre`) as xx'))->get();
+
+                $query['categoriasinagua'] = DB::table(DB::raw('(select v7.nombre as categoria, count(v1.id) as "sin_agua",cast(SUM(v1.unid_uso) as SIGNED) as "unid_uso"
+                from `viv_emapacopsa` as `v1` 
+                inner join `viv_tipo_servicio` as `v2` on `v2`.`id` = `v1`.`tipo_servicio_id`         
+                inner join `viv_estado_conexion` as `v3` on `v3`.`id` = `v1`.`estado_conexion_id`   
+                inner join `viv_manzana` as `v4` on `v4`.`id` = `v1`.`manzana_id`  
+                inner join `viv_sector` as `v5` on `v5`.`id` = `v4`.`sector_id` 
+                inner join `par_ubigeo` as `v6` on `v6`.`id` = `v5`.`ubigeo_id` 
+                inner join `viv_sub_categoria` as `v7` on `v7`.`id` = `v1`.`sub_categoria_id`    
+                WHERE `v1`.`importacion_id`=' . $importacion_id . ' and `v1`.`tipo_servicio_id` in(3)
+                group by `v7`.`nombre`) as xx'))->get();
                 return $query;
             case 25:
                 $estadoconexion = '';
@@ -66,6 +87,27 @@ class EmapacopsaRepositorio
                     ->groupBy('xx.servicio')
                     //->orderBy('xx.servicio','desc')
                     ->get();
+                $query['categoriaconagua'] = DB::table(DB::raw('(select v7.nombre as categoria, count(v1.id) as "con_agua",cast(SUM(v1.unid_uso) as SIGNED) as "unid_uso"
+                from `viv_emapacopsa` as `v1` 
+                inner join `viv_tipo_servicio` as `v2` on `v2`.`id` = `v1`.`tipo_servicio_id`         
+                inner join `viv_estado_conexion` as `v3` on `v3`.`id` = `v1`.`estado_conexion_id`   
+                inner join `viv_manzana` as `v4` on `v4`.`id` = `v1`.`manzana_id`  
+                inner join `viv_sector` as `v5` on `v5`.`id` = `v4`.`sector_id` 
+                inner join `par_ubigeo` as `v6` on `v6`.`id` = `v5`.`ubigeo_id` 
+                inner join `viv_sub_categoria` as `v7` on `v7`.`id` = `v1`.`sub_categoria_id`    
+                WHERE `v1`.`importacion_id`=' . $importacion_id . ' and `v1`.`tipo_servicio_id` in(2,3) 
+                group by `v7`.`nombre`) as xx'))->get();
+
+                $query['categoriasinagua'] = DB::table(DB::raw('(select v7.nombre as categoria, count(v1.id) as "sin_agua",cast(SUM(v1.unid_uso) as SIGNED) as "unid_uso"
+                from `viv_emapacopsa` as `v1` 
+                inner join `viv_tipo_servicio` as `v2` on `v2`.`id` = `v1`.`tipo_servicio_id`         
+                inner join `viv_estado_conexion` as `v3` on `v3`.`id` = `v1`.`estado_conexion_id`   
+                inner join `viv_manzana` as `v4` on `v4`.`id` = `v1`.`manzana_id`  
+                inner join `viv_sector` as `v5` on `v5`.`id` = `v4`.`sector_id` 
+                inner join `par_ubigeo` as `v6` on `v6`.`id` = `v5`.`ubigeo_id` 
+                inner join `viv_sub_categoria` as `v7` on `v7`.`id` = `v1`.`sub_categoria_id`    
+                WHERE `v1`.`importacion_id`=' . $importacion_id . ' and `v1`.`tipo_servicio_id` in(1)
+                group by `v7`.`nombre`) as xx'))->get();
                 return $query;
 
             default:
